732fa1808a8d0b921d8e362b3f79e63c836bbf86
RichDi <drdr_2@hotmail.com>
2017-10-05 22:34:42 -0500

    Inicializar Proyecto y agregado index

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    template = 'index.html'
+    context={
+    	'title': "Home",
+    }
+    return render(request, template, context)

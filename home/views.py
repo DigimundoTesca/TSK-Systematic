05d1ee3570ff90ad63bbdc6459b9aecd26d31b81
RichDi <drdr_2@hotmail.com>
2017-10-31 00:17:32 -0600

    Agregado Materialize-Autocomplete a Marcas

--- home/views.py
+++ home/views.py
@@ -1,12 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from home.models import marca, modelo
+import json
 
 def index(request):
     template = 'index.html'    
-    context={
-        'form': form,
+    marca_a=marca.objects.all()
+    modelo_a=modelo.objects.all()
+
+    list_Marca= []
+    list_Modelo=[]
+    countmarc = 0;
+    countmod = 0;
+
+    for marc in marca_a:
+        name = marc.nombreMarca.replace('\'','')         
+        list_Marca.append(name)
+
+    for mod in modelo_a: 
+        name = mod.nombreModelo.replace('\'','')         
+        list_Modelo.append(name)    
+
+
+    marca_json = json.dumps(list_Marca)
+    modelo_json = json.dumps(list_Modelo)
+    
+    context={        
         'title': "Home",
+        'marca': marca_json,
+        'modelo': modelo_json,
     }
 
     return render(request, template, context)

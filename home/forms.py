d5656bc08aa1df1893b189f26789a268ba3b990f
RichDi <drdr_2@hotmail.com>
2017-11-14 15:42:26 -0600

    Se modificó el form para el modal y se envía correctamente por correo. Falta diseño para el correo

--- home/forms.py
+++ home/forms.py
@@ -1,10 +1,8 @@
 from django import forms
 
-
-
 class ContactForm(forms.Form):
-    contact_name = forms.charfied( max_length=30 )
-    contact_email = forms.charfied(max_length=50 )
-    contact_phone = forms.numberfield(max_length=10)
-    contact_zone = forms.charfied(max_length=40)
-    contact_comment = forms.charfied(max_length=50 )
+    contact_name = forms.CharField(max_length=30)
+    contact_email = forms.CharField(max_length=50)
+    contact_phone = forms.CharField(max_length=10)
+    contact_zone = forms.CharField(max_length=40)
+    contact_comment = forms.CharField(max_length=50)
--- home/views.py
+++ home/views.py
@@ -3,18 +3,38 @@ import smtplib
 from django.shortcuts import render
 from home.models import marca, modelo
 from django.core.mail import send_mail
-from collection.forms import ContactForm
 from django.core.mail import EmailMessage
 from django.shortcuts import redirect
 from django.template import Context
 from django.template.loader import get_template
-from forms import ContactForm
-
+from home.forms import ContactForm
 
 import json
 
 def index(request):
+
     template = 'index.html'
+
+    form = ContactForm()
+
+    if request.method == 'POST':
+        contact_name = request.POST['contact_name']
+        contact_email = request.POST['contact_email']
+        contact_phone = request.POST['contact_phone']
+        contact_zone = request.POST['contact_zone']
+        contact_comment = request.POST['contact_comment']
+
+        fromaddr = 'tescauber@gmail.com'
+        toaddrs = 'a.g.tornell@outlook.com'
+        msg = contact_name +" "+ contact_email +" "+ contact_phone +" "+ contact_zone +" "+ contact_comment
+        username = 'tescauber@gmail.com'
+        password = 'yosoytesca123'
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(username, password)
+        server.sendmail(fromaddr, toaddrs, msg)
+        server.quit()
 
     marca_a=marca.objects.all()
     modelo_a=modelo.objects.all()
@@ -39,18 +59,18 @@ def index(request):
     marca_json = json.dumps(list_Marca)
     modelo_json = json.dumps(list_Modelo)
 
+    
+
     context={
         'title': "Home",
+        'form':form,
         'marca_obj': marca_a,
         'modelo_obj': modelo_a,
         'marca': marca_json,
         'modelo': modelo_json,
     }
-    def contact(request):
-    form_class = ContactForm    
-    return render(request, 'contact.html', {
-        'form': form_class,
-    })
+    
+    return render(request,template,context)
 
 
     """fromaddr = 'tescauber@gmail.com'
@@ -109,22 +129,4 @@ def index(request):
 
     https://hellowebbooks.com/news/tutorial-setting-up-a-contact-form-with-django/"""
 
-    def ContactForm(request):
-        if request.method == 'POST':
-            contact_name = request.POST['contact_name']
-            contact_email = request.POST['contact_email']
-            contact_phone = request.POST['contact_phone']
-            contact_zone = request.POST['contact_zone']
-            contact_comment = request.POST['contact_comment']
-
-            print contact_name
-            print contact_email
-            print contact_phone
-            print contact_zone
-            print contact_comment
-
-        form = ContactForm()
-        context = {
-        'form' : form
-        }
-        return render (request, 'formulario.html', context )
+    
